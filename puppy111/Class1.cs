ea280c4121540ab0fea24d61ddc6cfbf93a1d925
jyothishettychinna <jyothi.vtalent@gamil.com>
2019-08-28 18:57:36 +0530

    class  add to branch1

---
+++ puppy111/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puppy111
+{
+    class Class1
+    {
+        public int add()
+        {
+            int a = 10;
+            int b = 20;
+            return a + b;
+        }
+    }
+}
