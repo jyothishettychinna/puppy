d3faafb556c57201b1b41f1184fd469609cc6097
jyothishettychinna <jyothi.vtalent@gamil.com>
2019-08-28 19:05:47 +0530

    branch2 class added to prject

---
+++ puppy111/br2Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puppy111
+{
+    class br2Class
+    {
+        public int sub()
+        {
+            int x = 10;
+            int y = 5;
+            return x - y;
+        }
+    }
+}
